1d60314ff2cc506d41ac87bd14773fbb3ad31548
unsurv <43016521+unsurv@users.noreply.github.com>
2020-08-28 03:03:01 +0200

    added csv format for bulk JOSM upload

--- map_helper.js
+++ map_helper.js
@@ -83,18 +83,16 @@ function populateMap(camerasToShow, osm) {
 function focusView(positionInCamerasArray) {
 
   console.log("focusViewID: " + positionInCamerasArray);
-  console.log(markers);
   var camera = cameras[positionInCamerasArray];
   var tmp_marker = markers[positionInCamerasArray];
-  console.log(tmp_marker);
+
 
   var lat = tmp_marker.getLatLng().lat;
   var lon = tmp_marker.getLatLng().lng;
 
   tmp_marker.openPopup();
-  map.setZoom(16);
-  map.panTo([lat, lon]);
 
+  map.panTo([lat, lon]);
 
 }
 
@@ -137,4 +135,147 @@ function redrawMarker(positionInCamerasArray) {
   ).openPopup();
 
 
+}
+
+// download osm cameras from overpass-api.de
+// this data is helpful for determining whether a datapoint already exists in the OSM database
+
+// https://overpass-api.de/api/interpreter?data=[out:json];node[man_made=surveillance](52.5082248,13.3780064,52.515041,13.3834472);out;
+
+function downloadOsmData() {
+
+  var mapBounds = map.getBounds();
+  var latitudeDistance = Math.abs(mapBounds.getSouth() - mapBounds.getNorth());
+  var longitudeDistance = Math.abs(mapBounds.getWest() - mapBounds.getEast());
+  console.log(latitudeDistance);
+  console.log(longitudeDistance);
+
+  if (latitudeDistance < 0.1 && longitudeDistance < 0.2) { // latitudeDistance ~ 10 km, // longitudeDistance changes with latitude
+    var area = [mapBounds.getSouth(), mapBounds.getWest(), mapBounds.getNorth(), mapBounds.getEast()];
+    console.log(area.toString());
+
+    fetch("https://overpass-api.de/api/interpreter?data=[out:json];node[man_made=surveillance](" + area.toString() + ");out meta;")
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.elements[0]);
+
+        osmCameras = data.elements;
+
+        populateMap(osmCameras, true);
+
+      });
+
+  } else {
+    alert("Please decrease mapsize!");
+  }
+
+
+}
+
+
+function downloadCsv() {
+
+  const types = {
+    "0": "fixed",
+    "1": "dome",
+    "2": "panning"
+  }
+
+  const areas = {
+    "0": "unknown",
+    "1": "outdoor",
+    "2": "public",
+    "3": "indoor",
+    "4": "traffic"
+  }
+
+  const mounts = {
+    "0": "unknown",
+    "1": "pole",
+    "2": "wall",
+    "3": "ceiling",
+    "4": "street_lamp"
+  }
+
+  console.log(mounts["0"]);
+
+  // var csv = 'TYPE,AREA,DIRECTION,MOUNT,HEIGHT,ANGLE,THUMBNAIL,IMAGE,LAT,LON,surveillance,UPLOAD\n';
+  var csv = '\"camera:type\",\"surveillance\",\"camera:direction\",\"camera:mount\",\"height\",\"camera:angle\",\"image\",\"latitude\",\"longitude\",\"surveillance:type\",\"man_made\"\n';
+
+  var data = cameras;
+
+  data.forEach(function(row) {
+    console.log(row);
+
+    // camera has been approved by user
+    if (row[10] == "1") {
+
+      var processedRow = "";
+
+      // type
+      processedRow += types[row[0]] + ",";
+
+      // area
+      var area = areas[row[1]];
+
+      if (area != "unknown") {
+        processedRow += area + ",";
+      } else {
+        processedRow += "" + ",";
+      }
+
+      // direction
+      if (row[2] != "-1") {
+        processedRow += row[2] + ",";
+      } else {
+        processedRow += "" + ",";
+      }
+
+      // mount
+      var mount = areas[row[3]];
+
+      if (mount != "unknown") {
+        processedRow += mount + ",";
+      } else {
+        processedRow += "" + ",";
+      }
+
+      // height
+      if (row[4] != "-1") {
+        processedRow += row[4] + ",";
+      } else {
+        processedRow += "" + ",";
+      }
+
+      // angle
+      processedRow += row[5] + ",";
+
+      // imageUrl
+      processedRow += "https://unsurv.org/" + row[6] + ",";
+
+      // latitude
+      processedRow += row[8] + ",";
+
+
+      // longitude
+      processedRow += row[9] + ",";
+
+      // surveillance
+      processedRow += "camera";
+      // man_made
+      processedRow += "surveillance";
+
+      processedRow += "\n";
+
+      csv += processedRow;
+
+    }
+
+  });
+
+  var hiddenElement = document.createElement('a');
+  hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
+  hiddenElement.target = '_blank';
+  hiddenElement.download = Math.random().toString(36).substr(2, 5) + '.csv';
+  hiddenElement.click();
 }
